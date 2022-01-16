40f5ad55e5c1130a4cd84b0304aa918f5fc7d13d
Jason Olson <jolson88@outlook.com>
2022-01-15 22:43:26 -0800

    Initial atom program

---
+++ c/0-atom/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+  printf("Hello, Jason!\n");
+  return 0;
+}
---
+++ c/0-atom/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+  printf("Hello, Tests!\n");
+  return 0;
+}
